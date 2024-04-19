4f33f6e09639092b71d1902de27520f966ec88f3
Brandon <brandon@mz.co.za>
2024-04-19 16:46:59 +0200

    update: theme

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
