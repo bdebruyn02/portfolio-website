44b1174a3accd9d8f6e9bfae52445ea8aa9d552a
Brandon <brandon@mz.co.za>
2024-04-20 11:23:45 +0200

    add: Skills section

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LinksT, ProfileComponent } from './components/profile/profile.component';
 import { EducationAndWorkHistoryComponent } from './components/education-and-work-history/education-and-work-history.component';
-import { SkillsAndProjectsComponent } from './components/skills-and-projects/skills-and-projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProfileComponent, EducationAndWorkHistoryComponent, SkillsAndProjectsComponent],
+  imports: [ProfileComponent, EducationAndWorkHistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,10 +13,13 @@ export class AppComponent {
   title = 'portfolio';
 
   links: Array<LinksT> = [
-    { name: 'View Github', href: 'https://github.com/LordTiger',icon: 'logo-github' },
-    { name: 'Download Resume', href: 'https://drive.google.com/file/d/1ivT9rXfcjPebL6HAlUwQv1k0iCfkuaff/view', icon:'cloud-download-outline'}
+    { name: 'View Github Profile', href: 'https://github.com/LordTiger', icon: 'logo-github' },
+    { name: 'Download Resume', href: 'https://drive.google.com/file/d/1ivT9rXfcjPebL6HAlUwQv1k0iCfkuaff/view', icon:'cloud-download-outline'},
+    { name: 'View Linkedin Profile', href: 'https://www.linkedin.com/in/bdebruyn/', icon: 'logo-linkedin'}
   ]
 
   about_me: string = `Hi, I'm Brandon, a full-stack developer with a strong passion for coding. My primary tech stack includes ASP.NET and ASP.NET Web API, .NET Framework, C#, TypeScript, JavaScript, Angular, 
-  SCSS, CSS, Ionic, Firebase, and Microsoft SQL. I strive for excellence in all my projects and am dedicated to staying updated with the latest advancements in technology.`
+  SCSS, CSS, Ionic, Firebase, and Microsoft SQL. I strive for excellence in all my projects and am dedicated to staying updated with the latest advancements in technology.`;
+
+  skills: Array<string> = ['Microsoft SQL', 'ASP.NET Web API', 'Angular', 'Firebase', 'JavaScript', 'TypeScript', "C#", 'Ionic', ".NET Core", "ASP.NET MVC", "Git"];
 }
--- portfolio/src/app/components/profile/profile.component.ts
+++ portfolio/src/app/components/profile/profile.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import { IonIcon } from "@ionic/angular/standalone";
+import { MatIconModule } from '@angular/material/icon';
+import { IonChip, IonIcon } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
-import { logoGithub, cloudDownloadOutline } from 'ionicons/icons';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDividerModule} from '@angular/material/divider'
+import { logoGithub, cloudDownloadOutline, logoLinkedin } from 'ionicons/icons';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDividerModule } from '@angular/material/divider'
 
 export type LinksT = {
   icon: string,
@@ -18,24 +18,25 @@ export type LinksT = {
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   standalone: true,
-  imports: [IonIcon, CommonModule, MatIconModule, MatTooltipModule, MatDividerModule]
+  imports: [IonIcon, CommonModule, MatIconModule, MatTooltipModule, MatDividerModule, IonChip]
 })
-export class ProfileComponent  implements OnInit {
-    @Input({required: true}) name!: string;
-    @Input({required: true}) jobTitle!: string;
-    @Input({required: true}) location!: string;
-    @Input({required: true}) links!: Array<LinksT>;
-    @Input({required: true}) about_me!: string;
+export class ProfileComponent implements OnInit {
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) jobTitle!: string;
+  @Input({ required: true }) location!: string;
+  @Input({ required: true }) links!: Array<LinksT>;
+  @Input({ required: true }) about_me!: string;
+  @Input({ required: true }) skills!: Array<string>;
 
-    window = window;
-    
+  window = window;
 
-    constructor() {
-      addIcons({logoGithub, cloudDownloadOutline});
-    }
+
+  constructor() {
+    addIcons({ logoGithub, cloudDownloadOutline, logoLinkedin });
+  }
 
   ngOnInit() {
-    
+
   }
 
 }
