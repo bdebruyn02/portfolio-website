fee892597056a9924c884c34a10ac8e42d2f3133
Brandon <brandon@mz.co.za>
2024-04-22 23:00:21 +0200

    add: implement base of education & experience component

--- portfolio/src/app/components/education-and-work-history/education-and-work-history.component.ts
+++ portfolio/src/app/components/education-and-work-history/education-and-work-history.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'education-and-work-history',
   templateUrl: './education-and-work-history.component.html',
   styleUrls: ['./education-and-work-history.component.scss'],
   standalone: true,
+  imports: [CommonModule, MatCardModule, MatDividerModule]
 })
 export class EducationAndWorkHistoryComponent  implements OnInit {
 
