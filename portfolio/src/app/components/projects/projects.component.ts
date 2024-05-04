4d992f5c1e0b49de6a9bb2e68242ad9ff2fd08f0
Brandon de Bruyn <31498965+LordTiger@users.noreply.github.com>
2024-05-04 18:35:02 +0200

    add: carousel

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 export type ProjectT = {
   name: string;
@@ -14,11 +15,37 @@ export type ProjectT = {
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
   standalone: true,
-  imports: [ CommonModule, IonCardSubtitle, IonCardTitle, IonCardContent, IonCardHeader, IonCard ]
+  imports: [ CommonModule, IonCardSubtitle, IonCardTitle, IonCardContent, IonCardHeader, IonCard, CarouselModule ]
 })
 export class ProjectsComponent  implements OnInit {
   @Input({required: true}) projects!: Array<ProjectT>;
   window = window;
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    // dots: true,
+
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 
   constructor() { }
 
