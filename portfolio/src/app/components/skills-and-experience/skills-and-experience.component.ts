072175beab6c1dbd8f3a41da7c92097b8286c380
Brandon de Bruyn <31498965+LordTiger@users.noreply.github.com>
2024-05-11 17:05:31 +0200

    update: skills and experience component

--- portfolio/src/app/components/skills-and-experience/skills-and-experience.component.ts
+++ portfolio/src/app/components/skills-and-experience/skills-and-experience.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IonCard, IonCardContent } from '@ionic/angular/standalone';
+import { Chart, ChartModule } from 'angular-highcharts';
+import { Options, SeriesOptionsType } from 'highcharts';
 
 export type Skill_ExpT = {
   category: 'Backend Development' | 'Frontend Development' | 'Database and Deployment';
@@ -13,13 +15,80 @@ export type Skill_ExpT = {
   templateUrl: './skills-and-experience.component.html',
   styleUrls: ['./skills-and-experience.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonCard, IonCardContent, MatCardModule]
+  imports: [CommonModule, IonCard, IonCardContent, MatCardModule, ChartModule]
 })
 export class SkillsAndExperienceComponent  implements OnInit {
   @Input({required: true}) skills_exp!: Array<Skill_ExpT>;
   @Input({required: true}) description!: string;
-  constructor() { }
 
-  ngOnInit() {}
+  chartOptions: Options = {
+    chart: {
+      plotShadow: false,
+      type: 'pie'
+    },
+    
+    title: {
+      text: ''
+    },
+    accessibility: {
+      point: {
+          valueSuffix: '%'
+      }
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+      pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          borderRadius: 5,
+          dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+              distance: -50,
+              filter: {
+                  property: 'percentage',
+                  operator: '>',
+                  value: 4
+              }
+          }
+      }
+  },
+    series: []
+  };
+
+  chart: Chart = new Chart();
+
+  ngOnInit() {
+    this.setPieChartData();
+  }
+
+
+  private setPieChartData() {
+    const chartOptions = {...this.chartOptions};
+    const totalTech = this.skills_exp.flatMap(x => x.technologies).length;
+    let data: Array<object> = [];
+ 
+    this.skills_exp.forEach((x) => {
+      const percentage = (x.technologies.length / totalTech) * 100;
+
+      data.push({
+        name: x.category,
+        y: percentage
+      })
+    });
+
+    chartOptions.series = [
+      {
+        name: 'Percentage',
+        colorByPoint: true,
+        data: data
+
+      } as SeriesOptionsType
+    ];
+
+    this.chart = new Chart(chartOptions);
+  }
 
 }
