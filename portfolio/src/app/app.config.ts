9f2ae177c2ef569435b8edfc0a2d14242e39f03e
Brandon de Bruyn <brandon.debruyn@cbos.co.za>
2024-09-06 14:34:24 +0200

    reset

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,12 +1,8 @@
-import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), NG_EVENT_PLUGINS]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
