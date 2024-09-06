178e6519808ef189f8525c620366b4c1dd1bb268
Brandon de Bruyn <brandon.debruyn@cbos.co.za>
2024-09-06 14:49:32 +0200

    c

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnimationComponent } from "../../components/animation/animation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, AnimationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideLottieOptions } from 'ngx-lottie';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), NG_EVENT_PLUGINS]
+  providers: [
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideLottieOptions({
+      player: () => import('lottie-web'),
+    }),
+    NG_EVENT_PLUGINS,
+  ],
 };
