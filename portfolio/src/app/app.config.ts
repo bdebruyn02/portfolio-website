d3128ca7f47ee0ed97a5abe3f474a20a2dfdcdcb
Brandon de Bruyn <brandon.debruyn@cbos.co.za>
2024-09-07 10:25:54 +0200

    add: ionic

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideIonicAngular({})]
 };
