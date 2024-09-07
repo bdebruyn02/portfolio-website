fff920b4c1bb5894501953578e8698beeb4e1f11
Brandon de Bruyn <brandon.debruyn@cbos.co.za>
2024-09-08 00:10:50 +0200

    add: prime ng, carousel

--- portfolio/src/app/components/featured-projects/featured-projects.component.ts
+++ portfolio/src/app/components/featured-projects/featured-projects.component.ts
@@ -1,19 +1,44 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
 import { IonCard, IonCardContent } from "@ionic/angular/standalone";
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { projectT } from '../../types/types';
+import { CarouselModule, CarouselResponsiveOptions } from 'primeng/carousel';
+
 
 @Component({
   selector: 'featured-projects',
   templateUrl: './featured-projects.component.html',
   styleUrls: ['./featured-projects.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonCard, IonCardContent, MatCardModule, MatIconModule]
+  imports: [CommonModule, IonCard, IonCardContent, MatCardModule, MatIconModule, CarouselModule]
 })
 export class FeaturedProjectsComponent implements OnInit{
-  featuredProjects = input<projectT[]>();
+  @Input({required: true}) featuredProjects!: projectT[];
+
+  responsiveOptions: CarouselResponsiveOptions[] = [
+    {
+        breakpoint: '1920px',
+        numVisible: 4,
+        numScroll: 1
+    },
+    {
+        breakpoint: '1536px',
+        numVisible: 3,
+        numScroll: 1
+    },
+    {
+      breakpoint: '1440px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1
+  }
+  ];
 
   private window = window;
 
