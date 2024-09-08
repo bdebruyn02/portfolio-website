4d279cdc9bd670182668822531c1b50fe5bedd5d
Brandon de Bruyn <brandon.debruyn@cbos.co.za>
2024-09-08 11:22:15 +0200

    update: routing

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./page/home-page/home-page.component').then(m => m.HomePageComponent), title: 'Brandon de Bruyn - Resume' },
+    { path: '**', redirectTo: '', },
 ];
