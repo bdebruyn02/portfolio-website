9dce4d3e88d9372c4e035181d0dff06305eca382
Brandon de Bruyn <pomp.enteral.06@icloud.com>
2025-06-21 10:42:52 +0200

    remove: mdx

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
-
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +9,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), mdx()]
+  integrations: [icon()]
 });
